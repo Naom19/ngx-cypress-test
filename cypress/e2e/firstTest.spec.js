9fba58964ee2b35263333712f0dfaab303b740b7
Naom19 <naosan25@protonmail.com>
2023-09-12 17:33:19 -0600

    test: tooltips

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -265,4 +265,14 @@ it('Web tables', ()=> {
 })
 
 
+it('Tooltips', ()=> {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Tooltip').click()
+    cy.contains('nb-card', 'Colored Tooltips')
+    .contains('Default').click()
+    cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+})
+
+
 })
