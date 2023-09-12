4bfd6789bb76d4da8b7d132c77a5152c24466d89
Naom19 <naosan25@protonmail.com>
2023-09-12 17:28:53 -0600

    test: list and dropdowns

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -130,4 +130,103 @@ describe('Our first suite', () => {
         })
     })
 
+    
+    it('radio button', () =>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons =>{
+            cy.wrap(radioButtons)
+            .first()
+            .check({force:true})
+            .should('be.checked')
+        cy.wrap(radioButtons)
+            .eq(1)
+            .check({force:true})
+        cy.wrap(radioButtons)
+            .first() //it could be replaced with eq(0) index0
+            .should('not.be.checked')
+            cy.wrap(radioButtons)
+            .eq(2)
+            .should('be.disabled')
+        })
+    })
+
+    it('check boxes', () =>{
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+        cy.get('[type="checkbox"]').check({force:true})
+        cy.get('[type="checkbox"]').eq(0).click({force:true})
+    })
+//Check method is only to work with the elements of type checkbox and radio buttons.
+//It will not work on any other type of the elements. Can pick and check multiple checkboxes, but cannot
+//uncheck the checkbox
+//You can also use click method to work with checkboxes and radio buttons
+//Datepicker
+
+it("assert property", () => {
+    function selectDayFromCurrent(day) {
+        let date = new Date();
+        date.setDate(date.getDate() + day);
+        let futureDay = date.getDate();
+        let futureMonth = date.toLocaleString("default", { month: "short" }); // .getMonth() returns the number of the month, agregamos
+        let dateAssert = futureMonth + " " + futureDay + ", " + date.getFullYear();
+        cy.get("nb-calendar-navigation")
+          .invoke("attr", "ng-reflect-date")
+          .then((dateAttribute) => {
+            if (!dateAttribute.includes(futureMonth)) {
+              cy.get(' [data-name="chevron-right"] ').click();
+              selectDayFromCurrent(day);
+            } else {
+              cy.get(
+                'nb-calendar-day-picker [class="day-cell ng-star-inserted"]'
+              )
+                .contains(futureDay)
+                .click();
+            }
+          })
+          return dateAssert
+      }
+  cy.visit("/");
+  cy.contains("Forms").click();
+  cy.contains("Datepicker").click();
+  cy.contains("nb-card", "Common Datepicker")
+    .find("input")
+    .then((input) => {
+      cy.wrap(input).click();
+      let dateAssert = selectDayFromCurrent(3);
+  cy.wrap(input).invoke("prop", "value").should("contain", dateAssert);
+    });
+});
+
+it('list and dropdowns', () => {
+  cy.visit('/');
+  //1
+  /*   cy.get('nav nb-select').click()
+    cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+    cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')  */
+  //2
+  cy.get('nav nb-select').then((dropdown) => {
+    cy.wrap(dropdown).click();
+    cy.get('.options-list nb-option').each((listItem, index) => {
+      const itemText = listItem.text().trim();
+      const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)",
+      };
+      cy.wrap(listItem).click()
+      cy.wrap(dropdown).should('contain', itemText);
+      cy.get('nb-layout-header nav').should("have.css", "background-color", colors[itemText])
+      if (index < 3) {
+        cy.wrap(dropdown).click();
+      }
+    });
+  });
+});
+
+
 })
